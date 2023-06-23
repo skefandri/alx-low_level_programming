5afb2dd67cbad2c0e03f1af16ea9570989ef70ab
youssef <youssefsabr57@gmail.com>
2023-06-23 03:37:58 -0700

    asd

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - get the factor of a number
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	long	nb;
+	long	max;
+	long	save;
+	long	save2;
+
+	nb = 612852475143;
+	max = 0;
+	save = 0;
+	while (1)
+	{
+		if (max < save)
+			max = save;
+		if (nb == 1)
+			break;
+		save2 = 1;
+		while (++save2 <= nb)
+		{
+			if (nb % save2 == 0)
+			{
+				nb /= save2;
+				save = save2;
+				break;
+			}
+		}
+	}
+	printf("%ld\n", max);
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,44 @@
+#include <unistd.h>
+
+/**
+ * ft_putchar - print char
+ * @c: char that i want print it
+ *
+ * Return: Nothing.
+ */
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+/**
+ * main - start of the code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int	i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i != 1)
+			write(1, " ", 1);
+		if (i % 3 == 0)
+			write(1, "Fizz", 4);
+		if (i % 5 == 0)
+			write(1, "Buzz", 4);
+		if (i % 3 && i % 5)
+		{
+			if (i >= 10)
+				ft_putchar(i / 10 + 48);
+			ft_putchar(i % 10 + 48);
+		}
+		i++;
+	}
+	write(1, "\n", 1);
+	return (0);
+}
